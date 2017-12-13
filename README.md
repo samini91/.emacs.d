8ed79bad5650e6da681b6b0f56fe6a56f5828ef1
Sadiq <samini91@gmail.com>
2017-12-12 21:56:40 -0500

    init.el automatic setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Emacs-Init
+# C:\Users\{UserName}\AppData\Roaming\.emacs.d
--- init.el
+++ init.el
@@ -1,40 +1,29 @@
-(require 'package)
+(package-initialize)
 
+(require 'package)
+(require 'use-package)
+
+;;;;;;;;;;;;Melpa;;;;;;;;;;;;;;;;
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
        (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
   (add-to-list 'package-archives (cons "melpa" url) t))
 (when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
+ ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
-
-
+;;Stable
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
-(add-hook 'csharp-mode-hook 'omnisharp-mode)
-(eval-after-load
- 'company
- '(add-to-list 'company-backends 'company-omnisharp))
 
-;;(require 'omnisharp-settings)
-;;(require 'omnisharp-utils)
+;;;;;;;;;;;; Automatically downloads "use-package" packages if missing;;;;;;;;;;
+(setq use-package-always-ensure t)
+(use-package key-chord)
+(use-package company)
 
-(setq omnisharp-server-executable-path "/usr/local/bin/omnisharp")
+;;;;;;;;;;;;;;;;;;;KeyChords;;;;;;;;;;;;;;;;;;
 
-;; Removes Splash Screen
-(setq inhibit-startup-message t)
-;;Set title frame
-(setq frame-title-format '("GorgeousSexyMan"))
-;; remove bars
-(menu-bar-mode -1)
-(toggle-scroll-bar -1)
-(tool-bar-mode -1)
-
-(add-hook 'diff-mode-hook (lambda () (setq truncate-lines t)))
-
-;; Move more quickly up and down
 (global-set-key (kbd "M-n")
             (lambda ()
               (interactive)
@@ -45,20 +34,14 @@
               (interactive)
               (ignore-errors (previous-line 5))))
 
-(defun init-file ()
-  "Edit the `user-init-file', in another window."
-  (interactive)
-  (find-file-other-window user-init-file))
 
-
-
-(require 'key-chord)
 (key-chord-mode 1)
 (setq key-chord-two-keys-delay .040)
 
 (key-chord-define-global ";s" 'switch-to-buffer)
 (key-chord-define-global ";w" 'other-window)
 
+;;;;;;;;;;;;;;;;;;;;Company-Mode;;;;;;;;;;;;;;;;;;
 (add-hook 'after-init-hook 'global-company-mode)
   (defun my-csharp-mode-setup ()
     (setq indent-tabs-mode nil)
@@ -70,13 +53,38 @@
     (setq evil-shift-width 4)
     (local-set-key (kbd "C-c C-c") 'recompile))
 
-  (add-hook 'csharp-mode-hook 'my-csharp-mode-setup t)
 
+;;;;;;;;;;;;;;;;;;;;;;C Sharp Mode;;;;;;;;;;;;;;;;;;;;;;
+(add-hook 'csharp-mode-hook 'my-csharp-mode-setup t)
 
+(add-hook 'csharp-mode-hook 'omnisharp-mode)
+(eval-after-load
+ 'company
+ '(add-to-list 'company-backends 'company-omnisharp))
+
+;;;;;;;;;;;;; Miscelanous Functions ;;;;;;;;;;;;;;
+
+;; Causes buffer to always have the latest version (if using an external editor)
 (global-auto-revert-mode t)
 
+;; Jump to Init File
+(defun init-file ()
+  "Edit the `user-init-file', in another window."
+  (interactive)
+  (find-file-other-window user-init-file))
 
+;; Removes Splash Screen
+(setq inhibit-startup-message t)
+;;Set title frame
+(setq frame-title-format '("Gorgeous"))
+;; remove bars
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+;; No Word Wrap
+(add-hook 'diff-mode-hook (lambda () (setq truncate-lines t)))
 
+;;;;;;;;;;;;; Custom Theme ;;;;;;;;;;;;;;;
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
