4faf0aec8b9829b6480544404380dfefc42ad956
samini91 <samini91@gmail.com>
2018-03-31 13:55:01 -0400

    undescriptive commit

--- ensime.el
+++ ensime.el
@@ -7,13 +7,49 @@
 	  comment-style 'multi-line
 	  comment-empty-lines t)
 
+(defun switch-to-previous-buffer ()
+  "Switch to previously open buffer.
+Repeated invocations toggle between the two most recently open buffers."
+  (interactive)
+  (switch-to-buffer (other-buffer (current-buffer) 1)))
+
 (defun my-scala-mode-setup ()
   (setq comment-start "/* "
 	  comment-end " */"
 	  comment-style 'multi-line
 	  comment-empty-lines t)
-)
+  )
+
+(defun ensime-startandevalbuffer()
+  (interactive)
+  (funcall 'ensime-inf-run-scala)
+  ;;(funcall 'switch-)
+  (funcall 'ensime-inf-eval-buffer)
+  )
 
 (define-key ensime-mode-map (kbd "C-c C-c") 'comment-region)
 (define-key ensime-mode-map (kbd "C-c C-d") 'uncomment-region)
+
+(key-chord-define ensime-mode-map ";e" 'ensime-print-errors-at-point)
+(key-chord-define ensime-mode-map ";t" 'ensime-type-at-point)
+
+;;(key-chord-define ensime-mode-map ";r" 'ensime-repl)
+;;(key-chord-define ensime-mode-map ";r" 'ensime-inf-run-scala)
+
+;;(key-chord-define ensime-mode-map "[r" 'ensime-inf-eval-buffer)
+
+;;(key-chord-define ensime-mode-map ";r"
+;;		  (lambda ()
+;;		    (ensime-inf-eval-buffer 1)
+;;		    (ensime-inf-run-scala 1)))
+
+;;(key-chord-define ensime-mode-map ";r" 'ensime-inf-eval-buffer)
+;;(key-chord-define ensime-mode-map "[s" 'ensime-inf-run-scala)
+
+;;(define-key ensime-mode-map (kbd "C-t") 'ensime-type-at-point)
+
+
 (add-hook 'scala-mode-hook 'my-scala-mode-setup t)
+(add-hook 'scala-mode-hook 'ensime)
+
+(add-hook 'java-mode-hook 'ensime)
