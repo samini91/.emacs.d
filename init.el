f7009152288e78a6b8f3006497ecfd077ee669dc
samini91 <samini91@gmail.com>
2018-10-02 16:21:37 -0400

    Added C-g C-g for magit

--- init.el
+++ init.el
@@ -87,6 +87,7 @@ package-archive-priorities '(("melpa" . 1)))
 (global-set-key (kbd "C-x rl") 'helm-bookmarks )
 
 (global-set-key (kbd "C-x g") 'magit-status )
+(global-set-key (kbd "C-x C-g") 'magit-status ) 
 
 ;;(global-set-key (kbd "C-x <up>") 'windmove-up)
 (global-set-key (kbd "C-x k") 'windmove-up)
