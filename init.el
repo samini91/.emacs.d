bb6b12873d3787922a8693ce55b86a19266b5bd7
samini91 <samini91@gmail.com>
2019-05-15 09:39:22 -0400

    Removed Redundant Global Hook

--- init.el
+++ init.el
@@ -472,7 +472,6 @@ package-archive-priorities '(("melpa" . 1)))
 (use-package ace-window
   :config
   (key-chord-define-global ";w" 'ace-window)
-  (add-hook 'after-init-hook 'ace-window-mode)
   (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
   )
 (load-user-file "framemove.el")
