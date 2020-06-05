0c7d4b030c6727316f947284c9a1e42f4ab1c1d9
samini91 <samini91@gmail.com>
2020-06-04 21:05:03 -0400

    wrong slashes

--- init.el
+++ init.el
@@ -760,7 +760,7 @@ _~_: modified
 (defun nixos-file ()
   "Edit the `nixos configuration', in window."
   (interactive)
-  (find-file "\etc\nixos\configuration.nix"))
+  (find-file "/etc/nixos/configuration.nix"))
 
 
 ;;(display-time)
