51479be65f1ea0bc1319ada8462a0288959cd696
samini91 <samini91@gmail.com>
2023-12-13 03:40:23 +0000

    stuff

--- init.el
+++ init.el
@@ -55,13 +55,14 @@ package-archive-priorities '(("melpa" . 1)))
   (setq column-number-mode t)
   (add-hook 'after-init-hook (lambda ()
                                (load-theme 'doom-dark+ t)
-                               (custom-set-faces
+                               ;;(custom-set-faces
                                 ;; custom-set-faces was added by Custom.
                                 ;; If you edit it by hand, you could mess it up, so be careful.
                                 ;; Your init file should contain only one such instance.
                                 ;; If there is more than one, they won't work right.
                                 ;;'(face-remap-add-relative 'hl-line 'highlight 'bold))
-                                '(hl-line ((t (:extend t :background "#b6e3fc")))))
+
+                                ;;'(hl-line ((t (:extend t :background "#b6e3fc")))))
 
                                ))
   )
