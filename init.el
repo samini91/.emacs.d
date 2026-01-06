0327f785ab6d9adb772c2106166c95fea2743311
samini91 <samini91@gmail.com>
2026-01-06 03:18:00 +0000

    stuff

--- init.el
+++ init.el
@@ -186,6 +186,34 @@
 (define-key sa-tab-bar-map (kbd "l") 'tab-bar-history-forward)
 (define-key sa-tab-bar-map (kbd "C-l") 'tab-bar-history-forward)
 
+(use-package vterm :ensure t)
+
+(use-package inheritenv
+  :straight (:type git :host github :repo "purcell/inheritenv"))
+
+;; Try Out Claude
+
+(use-package monet
+  :straight (:type git :host github :repo "stevemolitor/monet" :branch "main" :depth 1
+                   :files ("*.el" (:exclude "images/*")))
+  )
+(use-package claude-code
+  :straight (:type git :host github :repo "stevemolitor/claude-code.el" :branch "main" :depth 1
+                   :files ("*.el" (:exclude "images/*")))
+  :bind-keymap
+  ("C-c c" . claude-code-command-map) ;; or your preferred key
+  ;; Optionally define a repeat map so that "M" will cycle thru Claude auto-accept/plan/confirm modes after invoking claude-code-cycle-mode / C-c M.
+  :bind
+  (:repeat-map my-claude-code-map ("M" . claude-code-cycle-mode))
+  :config
+  ;; optional IDE integration with Monet
+  (add-hook 'claude-code-process-environment-functions #'monet-start-server-function)
+  (monet-mode 1)
+  (claude-code-terminal-backend 'vterm)
+
+  (claude-code-mode))
+
+;;;
 ;;;;; Need to run (all-the-icons-install-fonts) for this to work properly
 (use-package doom-modeline
   :hook (after-init . doom-modeline-mode)
